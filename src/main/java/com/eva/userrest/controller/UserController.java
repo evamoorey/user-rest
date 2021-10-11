c97c889a201b339bb76eb7287c45ad8bd89e9d58
evamoorey <evgalyuta@edu.hse.ru>
2021-10-11 19:04:37 +0300

    add UserController interface

--- src/main/java/com/eva/userrest/controller/UserController.java
+++ src/main/java/com/eva/userrest/controller/UserController.java
@@ -1,64 +1,23 @@
 package com.eva.userrest.controller;
 
+
 import com.eva.userrest.entity.User;
-import com.eva.userrest.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
-@RestController
-@RequestMapping("/users")
-public class UserController {
+public interface UserController {
+    // Get all users.
+    List<User> usersData();
 
-    private final UserService userService;
+    // Get user by id from DB.
+    User getUserById(Integer id);
 
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+    // Add new user.
+    User addUser(User user);
 
+    // Delete user by id.
+    void deleteUser(Integer id);
 
-    @GetMapping
-    public String usersData() {
-        return userService.getAll().toString();
-    }
-
-    @GetMapping("{id}")
-    public @ResponseBody
-    String getUserById(@PathVariable Integer id) {
-        if (!userService.containsId(id)) {
-            return "Error! Database doesn't have user with this id.";
-        }
-        return userService.read(id).toString();
-    }
-
-    @PostMapping("/user/new")
-    public @ResponseBody
-    String addUser(@RequestBody User user) {
-        userService.save(user);
-        return "User added successfully!";
-    }
-
-    @GetMapping("user/delete/{id}")
-    public @ResponseBody
-    String deleteUser(@PathVariable Integer id) {
-        if (userService.containsId(id)) {
-            userService.delete(id);
-            return "User with id = " + id + " deleted successfully!";
-        }
-        return "Error! Database doesn't have user with this id.";
-    }
-
-    @PutMapping("user/update/{id}")
-    public @ResponseBody
-    String updateUser(@PathVariable Integer id, @RequestBody User user) {
-        if (userService.containsId(id)) {
-            userService.read(id).setName(user.getName());
-            userService.read(id).setInfo(user.getInfo());
-            userService.save(userService.read(id));
-            return "User with id = " + id + " changed successfully!";
-        }
-        return "Error! Database doesn't have user with this id.";
-    }
+    // Update user's info.
+    User updateUser(Integer id, User user);
 }
-
