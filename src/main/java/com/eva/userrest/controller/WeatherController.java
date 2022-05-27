2492cf4b0fdea50c5d5aa41428b5751f6bbf955b
evagalyuta <e.galiuta@mkskom.ru>
2022-05-27 14:59:06 +0300

    feat(api): delete some functions

--- src/main/java/com/eva/userrest/controller/WeatherController.java
+++
@@ -1,8 +0,0 @@
-package com.eva.userrest.controller;
-
-import com.eva.userrest.entity.WeatherData;
-import org.springframework.http.ResponseEntity;
-
-public interface WeatherController {
-    //ResponseEntity<WeatherData> getWeatherInCity(String id);
-}
--- src/main/resources/database/initDB.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS users
-(
-    Id SERIAL NOT NULL,
-    Name TEXT,
-    Info TEXT
-);
-INSERT INTO users (Name, Info) VALUES ('Peter', 'human');
-INSERT INTO users (Name, Info) VALUES ('Bob', 'dog');
